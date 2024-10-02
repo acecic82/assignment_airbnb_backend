b991d11072beb9821f81228f06a568656cf9ad59
acecic82 <102938al@naver.com>
2024-10-02 09:54:18 +0900

    Url / 에 들어가면 전체 트윗이 보여주도록 기능 추가

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from tweets.models import Tweet
 
 # Create your views here.
+
+
+def seeAllTweets(request):
+    allTweet = Tweet.objects.all()
+    return render(
+        request,
+        "allTweets.html",
+        {
+            "allTweet": allTweet,
+            "title": "See All Tweet",
+        },
+    )
